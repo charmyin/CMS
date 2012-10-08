8004ce32a5871f280ea8ac619a1cc4b49426a25c
charmyin <976582265@qq.com>
2012-10-08 21:25:02 +0800

    Generate random Code Picture has been done!

--- src/cn/com/charmyin/cms/backend/view/LoginServlet.java
+++ src/cn/com/charmyin/cms/backend/view/LoginServlet.java
@@ -5,7 +5,7 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.util.Random;
 
 import javax.servlet.ServletConfig;
@@ -16,7 +16,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import cn.com.charmyin.cms.backend.dao.AdminDao;
-import cn.com.charmyin.cms.backend.model.Admin;
+
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 public class LoginServlet extends BaseServlet {
 	private Logger logger = Logger.getLogger(this.getClass());
@@ -26,6 +28,19 @@ public class LoginServlet extends BaseServlet {
 	private int height;
 	private int number;//设置验证码长度
 	private String codes;//有哪些字符可以给选择
+	
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+		super.init(config);
+		
+		width=Integer.parseInt(config.getInitParameter("width"));
+		height = Integer.parseInt(config.getInitParameter("height"));
+		number = Integer.parseInt(config.getInitParameter("number"));
+		codes = config.getInitParameter("codes");
+	}
+	
 	/**
 	 * The doPost method of the servlet. <br>
 	 *
@@ -41,14 +56,14 @@ public class LoginServlet extends BaseServlet {
 
 		response.setContentType("image/jpeg");
 		
-		BufferedImage image = new BufferedImage(12,21,BufferedImage.TYPE_INT_RGB);
+		BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
 		Graphics2D g = image.createGraphics();
 		
 		g.setColor(Color.WHITE);
-		g.fillRect(0,0,width,height);
+		g.fillRect(0,0,width+width/4,height);
 		
 		g.setColor(Color.BLACK);
-		g.drawRect(0, 0, width, height);
+		g.drawRect(0, 0, width+width/4, height);
 		
 		Random random = new Random();
 		
@@ -68,8 +83,32 @@ public class LoginServlet extends BaseServlet {
 			
 			g.setColor(new Color(red,green,blue));
 			
-			Font font = new Font("Arial",Font.PLAIN,random(height/2,height));
+			Font font = new Font("Arial",Font.BOLD+Font.ITALIC,random(height/2,height-height/4));
+			
+			g.setFont(font);
+			double rd = random.nextInt(2)%2==0?random.nextDouble()/10:-random.nextDouble()/10;
+			g.rotate(rd);
+			g.drawString(code, i*x+1, Math.round(y*0.9d));
+			g.rotate(-rd);
+			sb.append(code);
 		}
+		
+		request.getSession().setAttribute("codes", sb.toString());
+		
+		for(int i=0; i<50; i++){
+			int red = random.nextInt(255);
+			int green = random.nextInt(255);
+			int blue = random.nextInt(255);
+			g.setColor(new Color(red,green,blue));
+			g.drawOval(random.nextInt(width), random.nextInt(height), 1, 1);
+		}
+		
+		OutputStream out = response.getOutputStream();
+		JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+		encoder.encode(image);
+		
+		out.flush();
+		out.close();
 	}
 
 	/**
@@ -83,16 +122,6 @@ public class LoginServlet extends BaseServlet {
 		return min+m;
 	}
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		
-		super.init(config);
-		
-		width=Integer.parseInt(config.getInitParameter("width"));
-		height = Integer.parseInt(config.getInitParameter("height"));
-		number = Integer.parseInt(config.getInitParameter("number"));
-		codes = config.getInitParameter("codes");
-	}
 
 	//用于注入adminDao
 	public void setAdminDao(AdminDao adminDao) {
